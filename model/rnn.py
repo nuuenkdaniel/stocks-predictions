4f28d97f4493047130a9736c8c3b0af6f3468773
xiangliu1103 <xiang.liu.1@stonybrook.edu>
2025-12-28 17:21:35 -0500

    RNN model

---
+++ model/rnn.py
@@ -0,0 +1,57 @@
+
+# for sentiment analysis, RNN is many to one (many words to one label of sentiment)
+# supervised learning because we have corresponding label to each text 
+
+import torch 
+import torch.nn as nn 
+
+class LSTM(nn.Module): 
+    # constructor, take the hyperparameters needed for the layers 
+    def __init__ (self, 
+                n_layers: int,  # number of layers at each time step 
+                embed_dim: int,     # embedding dimension to map word to number 
+                vocab_size: int,    # number of words in each inference to become number 
+                hidden_dim: int,    # hidden dimension (how much information the model can capture)
+                output_dim: int     # the dimension of output (based on what we are using the model for)
+                  ): 
+        super().__init__() 
+
+        # embedding layer 
+        # convers the words to a numerical value for model to process 
+        # trained with the model 
+        # vocab_size is the size of our dictionary, so this is alike a lookup table, where at each input data, the words become embedded 
+        # this is a look up table where the input is an one-hot encoding (index), and nn.Embedding retrieves its embedding dimension 
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+
+        # model inference 
+        # start with the embed dimension and start running the model on (matrix multiplication)
+        # each timestep has num_layers number of hidden layers, each hidden layer is of size hidden_dim 
+        # batch_size x seq_len x hidden_dim 
+        self.lstm = nn.LSTM(embed_dim, 
+                            hidden_size=hidden_dim,
+                            num_layers=n_layers,
+                            batch_first=True, 
+                            #bias=True,
+                            #dropout=0.5
+                            )
+        
+        # fully connected network to finalize our predictions 
+        # map the hidden layer output to diffrent outputs (neutral, postive, negative)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    # inference (running the model here)
+    def forward(self, x):
+        # first embed the input data 
+        embedded =self.embedding(x) 
+        
+        # pass through the lstm network 
+        # lstm_out is the hidden states of the last layer of every timestep (good for Many to Many)
+        # hidden is the final hidden states of every layer from the last timestep (after model processed the entire input)
+        # cell is the cell-state (internal cells) of each layer 
+        lstm_out, (hidden, cell) = self.lstm(embedded)
+
+        # get the last hidden state to do inference 
+        last_hidden = hidden[-1,:,:]
+
+        # do the final prediction (map the last layer)
+        return self.fc(last_hidden)

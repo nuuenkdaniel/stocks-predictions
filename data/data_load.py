516bce479675e42d1185a8b6fb410f1a894e81c0
xiangliu1103 <xiang.liu.1@stonybrook.edu>
2025-12-29 15:19:29 -0500

    tokenizer

--- data/data_load.py
+++ data/data_load.py
@@ -1,6 +1,7 @@
 import kagglehub
 import os 
 import pandas as pd 
+import numpy as np 
 
 def load_data(load_agreements=False): 
     '''
@@ -22,8 +23,46 @@ def load_data(load_agreements=False):
         return data_path, agreement_path
     else:
         return data_path, None  
+    
+
+def data_process(load_agreements=False): 
+    '''
+    Process the data to be trainable 
+    return the completed data in array format  
+
+    @param load_agreement: whether to load and process data that has different agreement levels 
+    '''
+    
+    # load data 
+    data_path, agreement_path = load_data(load_agreements)
+
+    # col 0: labels (neural, positive, negative)
+    # col 1: news text 
+    # 4846 x 2 
+    # 2879 neutral, 1363 positive, 604 negative 
+    df = pd.read_csv(data_path, encoding='latin-1', header=None, names=["sentiment", "news"]) 
+
+    ''' 
+    # iterate thorugh dataset to get additional info 
+    neutral= 0 
+    positive=0 
+    neg= 0 
+
+    # iterate thorugh each row as an Object 
+    for row in df.itertuples(): 
+        sent = row.sentiment 
+        if (sent=="neutral"): 
+            neutral +=1 
+        elif (sent== "negative"):
+            neg +=1 
+        else: 
+            positive+=1 
+    print(neutral)
+    print(positive)
+    print(neg)
+    ''' 
 
     
 
 if __name__ == "__main__": 
-    load_data() 
+    data_process()

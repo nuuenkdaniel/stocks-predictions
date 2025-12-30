4aa43562da89f53fef1624b423f93fd53919ea51
Daniel Lew <lewdaniel2@gmail.com>
2025-12-30 13:37:55 -0500

    FEAT: tokenizer

--- src/helpers/tokenizer.py
+++ src/helpers/tokenizer.py
@@ -1,15 +1,27 @@
-#!/bin/bash
-
 import sys
+import pandas as pd
+import re
 
+
+# Expects CSV
 class Tokenizer:
-    @staticmethod
-    def data_imputation():
-        pass
+    def __init__(self, file):
+        self.file = file
 
-    @staticmethod
-    def tokenize():
-        pass
+    def clean_data(self)-> pd.DataFrame:
+        df = pd.read_csv(self.file, encoding='ISO-8859-1', header=None, names=['label', 'headline'])
+        df = df.dropna().reset_index(drop=True)
+        df["label"] = df["label"].map({'negative': 0, 'neutral': 1, 'positive': 2}) # type: ignore
+        return df
+
+    def _tokenize(self, text: str) -> list[str]:
+        text = re.sub(r'[^\w\s]', '', text.lower())
+        return text.split()
+
+    def tokenized_df(self) -> pd.DataFrame:
+        df = self.clean_data()
+        df["tokens"] = df["headline"].apply(self._tokenize)
+        return df
 
 def usage():
     print("Usage:")
@@ -21,3 +33,5 @@ if __name__ == "__main__":
         exit(1)
 
     file = sys.argv[1]
+    tokenizer = Tokenizer(file)
+    print(tokenizer.tokenized_df())
